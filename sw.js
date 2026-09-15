84ddf726d4efa94b4fc63531bb71b6f05db77826
lukmanmanna77-svg <lukmanmanna77@gmail.com>
2026-09-16 07:52:24 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="imm-vnotch-v72";
+const CACHE="imm-vnotch-v73";
 const ASSETS=["./manifest.json","./icon-192.png","./icon-512.png","./banpu-heart.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
